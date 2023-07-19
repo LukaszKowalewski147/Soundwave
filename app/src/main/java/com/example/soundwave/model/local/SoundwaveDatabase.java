017176d7718e70b97814d6455caae47101424e19
Łukasz Kowalewski <kowalewski.ukasz@gmail.com>
2023-07-19 19:45:41 +0200

    (Room) Local database SoundwaveDatabase added.

---
+++ app/src/main/java/com/example/soundwave/model/local/SoundwaveDatabase.java
@@ -0,0 +1,30 @@
+package com.example.soundwave.model.local;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+import com.example.soundwave.model.dao.MusicDao;
+import com.example.soundwave.model.dao.ToneDao;
+import com.example.soundwave.model.entity.Music;
+import com.example.soundwave.model.entity.Tone;
+
+@Database(entities = {Tone.class, Music.class}, version = 1)
+public abstract class SoundwaveDatabase extends RoomDatabase {
+
+    private static SoundwaveDatabase instance;
+
+    public abstract ToneDao toneDao();
+    public abstract MusicDao musicDao();
+
+    public static synchronized SoundwaveDatabase getInstance(Context context) {
+        if (instance == null) {
+            instance = Room.databaseBuilder(context.getApplicationContext(), SoundwaveDatabase.class, "soundwave_database")
+                    .fallbackToDestructiveMigration()
+                    .build();
+        }
+        return instance;
+    }
+}
