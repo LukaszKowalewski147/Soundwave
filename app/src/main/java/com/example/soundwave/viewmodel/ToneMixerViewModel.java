9fddc869efb9fd29b003e492f979e8bb14d08812
Łukasz Kowalewski <kowalewski.ukasz@gmail.com>
2023-07-22 22:43:34 +0200

    View models for all fragments added.

---
+++ app/src/main/java/com/example/soundwave/viewmodel/ToneMixerViewModel.java
@@ -0,0 +1,28 @@
+package com.example.soundwave.viewmodel;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import com.example.soundwave.model.entity.Tone;
+import com.example.soundwave.model.repository.SoundwaveRepo;
+
+import java.util.List;
+
+public class ToneMixerViewModel extends AndroidViewModel {
+
+    private SoundwaveRepo repository;
+    private LiveData<List<Tone>> allTones;
+
+    public ToneMixerViewModel(@NonNull Application application) {
+        super(application);
+        repository = new SoundwaveRepo(application);
+        allTones = repository.getAllTones();
+    }
+
+    public LiveData<List<Tone>> getAllTones() {
+        return allTones;
+    }
+}
