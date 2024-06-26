adb447c37ebee1f08fa19be814e079bbd1f7df38
Łukasz Kowalewski <kowalewski.ukasz@gmail.com>
2024-06-26 02:18:32 +0200

    Bass guitar overtones preset done. Envelope presets update.

--- app/src/main/java/com/example/soundwave/utils/PresetEnvelope.java
+++ app/src/main/java/com/example/soundwave/utils/PresetEnvelope.java
@@ -3,10 +3,10 @@ package com.example.soundwave.utils;
 public enum PresetEnvelope {
     // PRESET_NAME {[0]-attack/ms, [1]-decay/ms, [2]-sustain level/%, [3]-sustain duration/ms, [4]-release/ms}
     FLAT(new int[]{100, 100, 50, 100, 100}),
-    PIANO(new int[]{500, 500, 50, 500, 500}),
-    GUITAR(new int[]{500, 500, 50, 500, 500}),
-    FLUTE(new int[]{500, 500, 50, 500, 500}),
-    TRUMPET(new int[]{500, 500, 50, 500, 500}),
+    PIANO(new int[]{10, 200, 70, 500, 200}),
+    GUITAR(new int[]{5, 0, 100, 100, 5000}),
+    FLUTE(new int[]{200, 50, 90, 500, 200}),
+    TRUMPET(new int[]{100, 50, 80, 500, 200}),
     CUSTOM(new int[]{500, 500, 50, 500, 500});
 
     public final int[] values;
