786059f6e709e44b5be7223e9fb34f5d0c2c4d4e
Łukasz Kowalewski <kowalewski.ukasz@gmail.com>
2024-08-13 21:10:17 +0200

    Envelope presets updated.

--- app/src/main/java/com/example/soundwave/utils/PresetEnvelope.java
+++ app/src/main/java/com/example/soundwave/utils/PresetEnvelope.java
@@ -2,11 +2,11 @@ package com.example.soundwave.utils;
 
 public enum PresetEnvelope {
     // PRESET_NAME {[0]-attack/ms, [1]-decay/ms, [2]-sustain level/%, [3]-sustain duration/ms, [4]-release/ms}
-    FLAT(new int[]{100, 100, 50, 100, 100}),
-    PIANO(new int[]{10, 200, 70, 500, 200}),
-    GUITAR(new int[]{5, 0, 100, 100, 5000}),
-    FLUTE(new int[]{200, 50, 90, 500, 200}),
-    TRUMPET(new int[]{100, 50, 80, 500, 200}),
+    FLAT(new int[]{0, 0, 100, 10000, 0}),
+    PIANO(new int[]{50, 0, 100, 500, 40000}),
+    GUITAR(new int[]{5, 0, 100, 250, 30000}),
+    FLUTE(new int[]{200, 700, 80, 10000, 250}),
+    TRUMPET(new int[]{300, 800, 80, 10000, 350}),
     CUSTOM(new int[]{500, 500, 50, 500, 500});
 
     public final int[] values;
