3d6e968d6f63c7aa053a290e82c273697377ac75
Łukasz Kowalewski <kowalewski.ukasz@gmail.com>
2024-08-15 21:48:18 +0200

    Settings activity: UI improvements.

--- app/src/main/java/com/example/soundwave/SettingsActivity.java
+++ app/src/main/java/com/example/soundwave/SettingsActivity.java
@@ -7,6 +7,10 @@ import android.os.Bundle;
 import android.view.MenuItem;
 
 import com.example.soundwave.databinding.ActivitySettingsBinding;
+import com.example.soundwave.utils.Options;
+
+import java.io.File;
+import java.util.Locale;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -20,6 +24,7 @@ public class SettingsActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         setupActionBar();
+        setupView();
     }
 
     @Override
@@ -40,5 +45,47 @@ public class SettingsActivity extends AppCompatActivity {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
             getSupportActionBar().setDisplayShowHomeEnabled(true);
         }
+    }
+
+    private void setupView() {
+        String tonesDir = Options.filepathToDownloadTones;
+        String musicDir = Options.filepathToDownloadMusic;
+
+        float tonesDirSize = getDirectorySizeInMB(tonesDir);
+        float musicDirSize = getDirectorySizeInMB(musicDir);
+
+        String tonesDirSizeText = String.format(Locale.US, "%.2f", tonesDirSize) + getString(R.string.unit_megabyte);
+        String musicDirSizeText = String.format(Locale.US, "%.2f", musicDirSize) + getString(R.string.unit_megabyte);
+
+        binding.settingsStorageTonesFilepath.setText(tonesDir);
+        binding.settingsStorageMusicFilepath.setText(musicDir);
+        binding.settingsStorageTonesDownloadedSize.setText(tonesDirSizeText);
+        binding.settingsStorageMusicDownloadedSize.setText(musicDirSizeText);
+    }
+
+    private float getDirectorySizeInMB(String directoryPath) {
+        File directory = new File(directoryPath);
+        long sizeInBytes = getDirectorySize(directory);
+
+        return sizeInBytes / (1024f * 1024f);   // conversion to MB
+    }
+
+    private long getDirectorySize(File directory) {
+        long length = 0;
+
+        if (directory.isDirectory()) {
+            File[] files = directory.listFiles();
+
+            if (files != null) {
+                for (File file : files) {
+                    if (file.isFile()) {
+                        length += file.length();
+                    } else {
+                        length += getDirectorySize(file);
+                    }
+                }
+            }
+        }
+        return length;
     }
 }
